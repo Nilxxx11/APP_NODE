dbe0076d3b892ac40e0206caa644de57e5f9fc42
Nilxxx11 <80600547+Nilxxx11@users.noreply.github.com>
2022-12-03 14:01:07 -0500

    Add files via upload

---
+++ BACKEND/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+const path = require('path');
+//Motor de plantillas_____________
+app.set('view engine', 'ejs');//##
+app.set('views',path.join(__dirname,'../FRONTEND/views'));
+//________________________________
+
+//Rutas__________________________
+app.get('/',(req,res)=>{
+  res.render('index', {namePage:'INICIO'});
+});
+app.get('/login',(req,res)=>{
+  res.render('login',{namePage:'LOGIN'});
+});
+//________________________________
+
+app.listen(port, (req, res)=> {
+  console.log(`server en el puerto ${port}`);
+});
